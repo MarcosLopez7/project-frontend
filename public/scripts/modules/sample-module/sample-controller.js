60d064e9641a4ce63927438e42ee2c761b4e5f1e
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-09-28 13:37:56 -0400

    controllers

--- public/scripts/modules/sample-module/sample-controller.js
+++ public/scripts/modules/sample-module/sample-controller.js
@@ -2,6 +2,23 @@ define(['angular', './sample-module'], function(angular, sampleModule) {
     'use strict';
     return sampleModule.controller('SampleCtrl', ['$scope', '$http', function($scope, $http) {
 
-        
+        function consumeService($scope, $http) {
+            $http({
+                method: 'GET',
+                url: 'https://monzalvos.run.aws-usw02-pr.ice.predix.io/canceled',
+                headers: {}
+            }).
+            success(function (response) {
+
+                $scope.data = response.cancelados;
+
+            }).
+            error(function (response) {
+                console.log(response);
+            });
+        }
+
+        consumeService($scope, $http);
+
     }]);
 });
--- public/scripts/modules/sample-module/sample-controller2.js
+++ public/scripts/modules/sample-module/sample-controller2.js
@@ -1,6 +1,24 @@
 define(['angular', './sample-module'], function(angular, sampleModule) {
     'use strict';
     return sampleModule.controller('SampleCtrl2', ['$scope', '$http', function($scope, $http) {
-        
+
+        function consumeService($scope, $http) {
+            $http({
+                method: 'GET',
+                url: 'https://monzalvos.run.aws-usw02-pr.ice.predix.io/engines',
+                headers: {}
+            }).
+            success(function (response) {
+
+                $scope.data = response.cancelados;
+
+            }).
+            error(function (response) {
+                console.log(response);
+            });
+        }
+
+        consumeService($scope, $http);
+
     }]);
 });
