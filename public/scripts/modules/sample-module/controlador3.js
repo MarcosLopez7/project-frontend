900e3c1916cb05eaee4be0c21905db8cbf546a5e
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-09-29 17:12:03 -0400

    casi listo

--- public/scripts/modules/sample-module/controlador3.js
+++ public/scripts/modules/sample-module/controlador3.js
@@ -4,6 +4,9 @@
 define(['angular', './sample-module'], function(angular, sampleModule) {
     'use strict';
     return sampleModule.controller('Ctrl3', ['$scope', '$http', function($scope, $http) {
+
+        $scope.cargado = false;
+
         function consumeService($scope, $http, numero) {
             $http({
                 method: 'GET',
@@ -26,10 +29,58 @@ define(['angular', './sample-module'], function(angular, sampleModule) {
                 });
                 $scope.min = min;
                 $scope.max = max;
-                $scope.temperaturas = arr;                
-
+                $scope.temperaturas = arr;
+                $scope.acumulado = response.acumulado;
+                $scope.num_vuelos = response.temperaturas.length;
+                getRuta($scope, $http, $scope.ruta);
             }).
             error(function (response) {
+                console.log(response);
+            });
+        }
+
+        function getRuta($scope, $http, numero) {
+            $http({
+                method: 'GET',
+                url: 'https://monzalvos.run.aws-usw02-pr.ice.predix.io/promedio/' + numero,
+                headers: {}
+            }).success(function (response) {
+                $scope.prom_temp = Math.round(response.promedio);
+                getResto($scope, $http);
+            }).error(function (response) {
+                console.log(response);
+            });
+
+        }
+
+        function getResto($scope, $http) {
+            $http({
+                method: 'GET',
+                url: 'https://monzalvos.run.aws-usw02-pr.ice.predix.io/minimum',
+                headers: {}
+            }).success(function (response) {
+                if ($scope.prom_temp > 0) {
+                    $scope.rutas_rest = Math.floor((parseInt(response.tiempo) - $scope.acumulado) / $scope.prom_temp);
+                    getReparacion($scope, $http, $scope.ruta);
+                } else {
+                    $scope.rutas_rest = "No hay registros de desgaste en esta ruta";
+                }
+            }).error(function (response) {
+                console.log(response);
+            });
+        }
+
+        function getReparacion($scope, $http, numero) {
+            $http({
+                method: 'GET',
+                url: 'https://monzalvos.run.aws-usw02-pr.ice.predix.io/ruta/' + numero,
+                headers: {}
+            }).success(function (response) {
+                $scope.lugar = response.minimo.ciudad;
+                $scope.costo = response.minimo.precio;
+                $scope.vuelos_rest = $scope.rutas_rest * response.ciudades.length;
+                $scope.cargado = true;
+            }).error(function (response) {
                 console.log(response);
             });
         }
